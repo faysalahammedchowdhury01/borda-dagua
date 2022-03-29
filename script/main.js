358cbad84a3c39ece4ef8c8f5fb0795bd6e477c2
Faysal <74490913+faysalchowdhury99@users.noreply.github.com>
2022-03-29 22:39:18 +0600

    back to top added

--- script/main.js
+++ script/main.js
@@ -33,3 +33,25 @@ function stopPropagationFunc(e) {
   console.log(e);
   e.stopPropagation();
 }
+
+/* back to top */
+const backToTopBtn = document.getElementById('back-to-top-btn');
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    backToTopBtn.style.display = 'block';
+  } else {
+    backToTopBtn.style.display = 'none';
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+backToTopBtn.addEventListener('click', topFunction);
